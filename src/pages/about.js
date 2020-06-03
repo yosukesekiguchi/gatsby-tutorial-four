99049297df60c0a0e24e55ca8700031c35d7ccd9
yosuke-sekiguchi <yosuke.s.5975@icloud.com>
2020-06-03 11:29:47 +0900

    ベースページを作成

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout"
+const About = () => {
+  return (
+    <Layout>
+      <h1>About Pandas Eating Lots</h1>
+      <p>
+        We're the only site running on your computer dedicated to showing the
+        best photos and videos of pandas eating lots of food.
+      </p>
+    </Layout>
+  )
+}
+
+export default About
